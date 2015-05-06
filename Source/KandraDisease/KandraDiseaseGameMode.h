fed29dd0f1eb3d062a41fa2a751c42d99c9622a1
MeaningIess <macke.holst@gmail.com>
2015-05-06 21:21:42 +0200

    Start

---
+++ Source/KandraDisease/KandraDiseaseGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "KandraDiseaseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KANDRADISEASE_API AKandraDiseaseGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
